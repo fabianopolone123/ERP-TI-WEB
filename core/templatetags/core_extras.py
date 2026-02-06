de60664919a079851eda88187000be56ba264211
fabiano <fabianopolone@gmail.com>
2026-02-06 13:03:30 -0300

    protege get_item contra string

--- core/templatetags/core_extras.py
+++ core/templatetags/core_extras.py
@@ -5,6 +5,6 @@ register = template.Library()
 
 @register.filter
 def get_item(mapping, key):
-    if mapping is None:
+    if mapping is None or isinstance(mapping, str):
         return []
     return mapping.get(key, [])
