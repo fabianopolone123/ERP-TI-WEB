b0550f6d11bc5c8ae8d0db542197146fe1bc4c9a
Fabiano Polone <fabiano.polone@SIDERTEC.INTRA.NET>
2026-02-10 17:05:25 -0300

    Email: atualizar remetente padrao para Chamados Sidertec

--- config/settings.py
+++ config/settings.py
@@ -181,7 +181,7 @@ EMAIL_PORT = int(os.environ.get('EMAIL_PORT', '587'))
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True').lower() in ('true', '1')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
-DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'ERP TI <no-reply@sidertec>')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'Chamados Sidertec <no-reply@sidertec>')
 
 # WhatsApp (WAPI)
 WAPI_DEFAULT_GROUP_JID = os.environ.get('WAPI_DEFAULT_GROUP_JID', '')
